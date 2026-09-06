fe192a7dc4babf26ff19fa4badec447c0929c05c
Bruno Matsuda <73135929+brunomatsuda@users.noreply.github.com>
2026-09-06 11:15:23 -0300

    feat: add LOAD

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from pathlib import Path
 ########## PATH ##########
 BASE_DIR = Path(__file__).resolve().parents[0]
 PATH_RAW_DATA = BASE_DIR/'data/raw/dados_abertos_banco_de_dados_de_madeiras_brasileiras_lpfsfb.csv'
+PATH_GOLD_DATA = BASE_DIR/'data/gold'
 
 ########## PARÂMETROS CSV ##########
 CSV_SEPARATOR = ";"
